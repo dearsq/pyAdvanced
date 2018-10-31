da1caeca131664de41e66f9c70743449bc083539
章扬 <foreveryounix@gmail.com>
2018-10-31 17:12:02 +0800

    将 score 隐藏, 使用 set_score 进行赋值

--- LearnOOP/learn_oop.py
+++ LearnOOP/learn_oop.py
@@ -13,13 +13,19 @@ def opp_print_score(std):
 class Student(object):
     def __init__(self,name,score):
         self.__name = name
-        self.__score = score
+        self.set_score(score)
 
     def get_name(self):
         return self.__name
 
     def get_score(self):
         return self.__score
+
+    def set_score(self,score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError('score beyonds range !')
 
     def oop_print_score(self):
         print('OOP %s: %s' % (self.__name, self.__score))
