ea91c8a157e4b21b6fa2e61a46292ec835ab5c0b
章扬 <foreveryounix@gmail.com>
2018-11-01 17:00:57 +0800

    Tips4: 定制类: 4.3 __iter__ 斐波那契数列

--- LearnOOP/learn_opp_advance.py
+++ LearnOOP/learn_opp_advance.py
@@ -192,3 +192,19 @@ print(s)
 print("__str__:",s.__str__())
 print("__repr__:",s.__repr__())
 
+print("============ __iter__ 迭代类 ============")
+class Fib(object):
+    def __init__(self):
+        self.a, self.b = 0, 1 # 初始化两个计数器a，b
+
+    def __iter__(self):
+        return self # 实例本身就是迭代对象，故返回自己
+
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b # 计算下一个值
+        if self.a > 100000: # 退出循环的条件
+            raise StopIteration()
+        return self.a # 返回下一个值
+
+for n in Fib():
+    print(n)
