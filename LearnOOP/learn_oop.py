a1c04367e7b6d3ea0205fef3ef6c0162cc85050e
章扬 <foreveryounix@gmail.com>
2018-11-01 10:19:54 +0800

    Tips1: 4. 类和属性 Tips2: 类属性, Student 每创建一个实例就自增

--- LearnOOP/learn_oop.py
+++ LearnOOP/learn_oop.py
@@ -138,3 +138,26 @@ def readImage(fp):
 
 readImage(mImage)   # has read
 readImage(mNumber)  # None
+
+print("============ 4. 类和属性 ============")
+class Student(object):
+    count = 0
+    def __init__(self, name):
+        self.name = name
+        Student.count += 1
+
+
+# 测试程序
+if Student.count != 0:
+    print('测试失败1!')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败2!')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败3!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
