c904a156b64b4e99950b7b090b6cb60d0d9fb719
章扬 <foreveryounix@gmail.com>
2018-11-01 10:42:28 +0800

    Tips3: property 用来将方法变为属性调用

--- LearnOOP/learn_opp_advance.py
+++ LearnOOP/learn_opp_advance.py
@@ -16,6 +16,9 @@ s.set_age(25) # 调用实例方法
 print(s.age) # 测试结果
 
 print("============ 限制实例添加属性 __slot__ ============")
+'''
+只对当前类有效, 对其子类无效
+'''
 class Student(object):
     __slots__ = ('name', 'age') #用tuple定义允许绑定的类型名称
 
@@ -24,6 +27,26 @@ s.name = 'Michael'
 print(s.name) # 'Michael' # 绑定属性'name'
 s.age = 25
 print(s.age)  # 25 # 绑定属性'age'
-s.score = 99
-print(s.score) #无法成功 # 绑定属性'score'
+if 0:
+    s.score = 99
+    print(s.score) #无法成功 # 绑定属性'score'
 # AttributeError: 'Student' object has no attribute 'score'
+
+print("============ @property ============")
+class Student(object):
+
+    def get_score(self):
+         return self._score
+
+    def set_score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+
+s = Student()
+s.set_score(60) # ok!
+print(s.get_score())
+# s.set_score(9999) #Error!
+# ValueError: score must between 0 ~ 100!
