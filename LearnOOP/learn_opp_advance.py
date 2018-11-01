de71567c00820e28d9124f78517a7a0b71b52680
章扬 <foreveryounix@gmail.com>
2018-11-01 10:43:00 +0800

    Tips3: property 用来将方法变为属性调用

--- LearnOOP/learn_opp_advance.py
+++ LearnOOP/learn_opp_advance.py
@@ -50,3 +50,24 @@ s.set_score(60) # ok!
 print(s.get_score())
 # s.set_score(9999) #Error!
 # ValueError: score must between 0 ~ 100!
+
+# property 实现
+class Student(object):
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+# Test
+s = Student()
+s.score = 60 # ok!
+print(s.score)
+# s.score = 9999 # Error!
+# ValueError: score must between 0 ~ 100!
