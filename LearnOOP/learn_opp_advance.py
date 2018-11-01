d552c9bd992688491ade13d6a3d0deee3f66a0fc
章扬 <foreveryounix@gmail.com>
2018-11-01 20:36:12 +0800

    Tips5: 枚举类

--- LearnOOP/learn_opp_advance.py
+++ LearnOOP/learn_opp_advance.py
@@ -244,3 +244,57 @@ class Fib2(object):
 
 f2 = Fib2()
 print(f2[0:5])
+
+print("============ 枚举类 ============")
+'''
+用枚举类实现月份
+'''
+from enum import Enum
+Month = Enum('月份',('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
+
+from enum import Enum , unique
+
+@unique #装饰器可以帮助我们检查保证没有重复值。
+class Weekday(Enum):
+    Sun = 0 # Sun的value被设定为0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+
+day1 = Weekday.Mon
+print(day1)
+
+print(Weekday.Tue)
+
+print(Weekday['Wed'])
+
+print(Weekday(4))
+
+for name, member in Weekday.__members__.items():
+    print(name, '=>', member)
+
+
+print("============ 枚举类 练习 ============")
+'''
+把Student的gender属性改造为枚举类型，可以避免使用字符串
+'''
+class Gender(Enum):
+    Male = 0
+    Female = 1
+
+class Student(object):
+    def __init__(self, name, gender):
+        self.name = name
+        self.gender = gender
+
+# 测试:
+bart = Student('Bart', Gender.Male)
+if bart.gender == Gender.Male:
+    print('测试通过!')
+else:
+    print('测试失败!')
